147e0744bbf1cc8a2d03e506048a9f770a60d1c0
Sergio <sergio.marretta@libero.it>
2021-03-16 19:11:46 +0100

    working

--- myscript.js
+++ myscript.js
@@ -1,6 +1,8 @@
 var randomNumbers = [];
 var numbers = [];
 var userNumbers = [];
+var num = [];
+userScore = 0;
 for (var i = 1; randomNumbers.length <= 4; i++) {
   var numbers = Math.floor(Math.random()* 10) + 1;
 if (randomNumbers.includes(numbers) == false) {
@@ -12,7 +14,33 @@ alert(randomNumbers);
 
 setTimeout(function () {
 for (var i = 1; i <= 5 ; i++) {
-  var userNumbers = prompt("Inserisci i numeri che ricordi");
-  console.log(userNumbers);
+  var num = parseInt(prompt("Inserisci i numeri"));
+ userNumbers.push(num);
+
+
 }
-}, 10000);
+
+if (randomNumbers.includes(userNumbers[0])) {
+  userScore += 1;
+
+}
+if (randomNumbers.includes(userNumbers[1])) {
+  userScore += 1;
+
+}
+if (randomNumbers.includes(userNumbers[2])) {
+  userScore += 1;
+
+}
+if (randomNumbers.includes(userNumbers[3])) {
+  userScore += 1;
+
+}
+if (randomNumbers.includes(userNumbers[4])) {
+  userScore += 1;
+
+}
+
+console.log("Punteggio: " + userScore );
+
+}, 30000);
